f255a40de0c09645b3d28c5ae5f8886d7410af34
Paz Spera <mspera03@gmail.com>
2023-05-08 00:00:52 -0300

    Agregué icono de FontAwesome en el navbar

--- cac-movies/src/App.jsx
+++ cac-movies/src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { LandingPage } from "./views/LandingPage";
 import { MovieDetail } from "./views/MovieDetail";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faXmarksLines } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+
+library.add(faXmarksLines);
 
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <nav className="navbar navbar-expand-lg navbar-dark">
+        <nav className="navbar navbar-expand-lg fixed-top bg-dark" data-bs-theme="dark">
           <div className="container">
             <Link to="/">
+              <FontAwesomeIcon icon={faXmarksLines} className="me-2" />
               <span>Movie Hive</span>
             </Link>
           </div>
